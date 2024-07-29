7e5f39f8710b0933eb92c974be2c8892ff9843ef
baibhavsingh08 <baibhav.singh@zopsmart.com>
2024-07-29 11:26:56 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Raramuri on 29/07/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
